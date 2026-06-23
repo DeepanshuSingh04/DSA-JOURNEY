2c72e1e042e2521d5abd81b073c064439012bf5c
Deepanshu Singh <deepanshusingh202206@gmail.com>
2026-06-24 00:57:01 +0530

    Add DigitsInAGivenNumber class to count digits

---
+++ Strings/DigitsInAGivenNumber.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class DigitsInAGivenNumber {
+    public static void main(String[] args) {
+        int x = 4483;
+        String s = Integer.toString(x);
+        System.out.println(s.length());
+    }
+}
