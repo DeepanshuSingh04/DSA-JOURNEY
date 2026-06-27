5e0622bcf576f9ae6252d56541734a2dd9932c8b
Deepanshu Singh <deepanshusingh202206@gmail.com>
2026-06-27 12:26:58 +0530

    Time: 1 ms (100%), Space: 46.7 MB (30.59%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.java
@@ -0,0 +1,49 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //reverse both list
+        ListNode head1 = reverse(l1);
+        ListNode head2 = reverse(l2);
+        ListNode t1 = head1;
+        ListNode t2 = head2;
+
+        int carry = 0;
+
+        //dummy node
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        //add two number
+        while(t1 != null || t2 != null || carry != 0){
+           int sum = carry;
+
+           if(t1 != null){
+            sum += t1.val;
+            t1 = t1.next;
+           }
+           if(t2 != null){
+            sum += t2.val;
+            t2 = t2.next;
+           }
+
+           carry = sum/10;
+           int digit = sum%10;
+           tail.next = new ListNode(digit);
+           tail = tail.next;
+        }
+        //reverse answer
+        return reverse(dummy.next);
+    }
+
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode fwd = null;
+        while(curr != null){
+            fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+        return prev;
+    }
+}
