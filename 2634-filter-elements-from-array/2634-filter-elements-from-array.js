b523c99a496fc147b813a561a8bb2d770444bf1f
Deepanshu Singh <deepanshusingh202206@gmail.com>
2026-06-28 23:36:09 +0530

    Time: 44 ms (55.35%), Space: 55.3 MB (5.89%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let ans = []
+
+    arr.forEach((value,index) => {
+        if(fn(value,index)){
+            ans.push(value);
+        }
+    });
+    return ans;
+};
