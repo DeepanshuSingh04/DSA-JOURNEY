2b9839f57e2b16a6c17c80514fc27e05429bc069
Deepanshu Singh <deepanshusingh202206@gmail.com>
2026-07-04 01:10:03 +0530

    Time: 28 ms (59.22%), Space: 102.5 MB (11.01%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,31 +1,75 @@
 class MinStack {
-    Stack<Integer> st;
-    Stack<Integer> minSt;
+    Stack<Long> st;
+    long min;
 
     public MinStack() {
         st = new Stack<>();
-        minSt = new Stack<>();
+        min = Long.MAX_VALUE;
     }
     
     public void push(int val) {
-         st.push(val);
-         if(minSt.size()==0 || val < minSt.peek())  minSt.push(val);
-         else minSt.push(minSt.peek());
+        if(st.size()==0) min = (long)val;
+
+        if((long)val >= min) st.push((long)val);
+        else{   // stack me fake value daal do
+            st.push((long)val + ((long)val-min));
+            min = (long)val;
+        }
     }
     
     public void pop() {
+        if(st.peek() < min){  //locha hain, minimum rollback karo
+            min = min + (min - st.peek()); 
+        }
         st.pop();
-        minSt.pop();
     }
     
     public int top() {
-        return st.peek();
+        long a = st.peek();
+        if(a < min){  //locha hain, minimum rollback karo
+            return (int)min;
+        }
+        else return (int)a;
     }
     
     public int getMin() {
-        return minSt.peek();
+        return (int)min;
     }
 }
+
+
+
+// class MinStack {
+//     Stack<Integer> st;
+//     Stack<Integer> minSt;
+
+//     public MinStack() {
+//         st = new Stack<>();
+//         minSt = new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//          st.push(val);
+//          if(minSt.size()==0 || val < minSt.peek())  minSt.push(val);
+//          else minSt.push(minSt.peek());
+//     }
+    
+//     public void pop() {
+//         st.pop();
+//         minSt.pop();
+//     }
+    
+//     public int top() {
+//         return st.peek();
+//     }
+    
+//     public int getMin() {
+//         return minSt.peek();
+//     }
+// }
+
+
+
 
 /**
  * Your MinStack object will be instantiated and called as such:
