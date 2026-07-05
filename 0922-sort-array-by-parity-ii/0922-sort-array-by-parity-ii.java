775586415daeb5494ef4037b7daf75b18287db02
Deepanshu Singh <deepanshusingh202206@gmail.com>
2026-07-06 00:05:51 +0530

    Time: 3 ms (58.08%), Space: 48.5 MB (67.25%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -4,11 +4,16 @@ class Solution {
         int odd = 1;
         int n = nums.length;
         while (even < n && odd < n) {
+//if even number at even place  found             
             if (nums[even] % 2 == 0) {
                 even += 2;
-            } else if (nums[odd] % 2 == 1) {
+            }
+//if odd number at odd place found           
+             else if (nums[odd] % 2 == 1) {
                 odd += 2;
-            } else {
+            } 
+//and if even place pr odd hua or odd place pr even number
+            else {
                 int temp = nums[even];
                 nums[even] = nums[odd];
                 nums[odd] = temp;
@@ -20,3 +25,5 @@ class Solution {
         return nums;
     }
 }
+
+//we will add 2 evertime in indexes so that keeps moving in odd and even respectives indexes
