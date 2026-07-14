a2b2b2b473367f8d19d4d1b03766ad241ccb72b7
Deepanshu Singh <deepanshusingh202206@gmail.com>
2026-07-14 23:46:12 +0530

    Time: 123 ms (11.44%), Space: 71.3 MB (12.62%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<=2) return 0;
+
+        int count = 0;
+        int[] s = seive(n);
+        for(int i=1; i<n; i++){
+        if(s[i]==1) count++;
+        }
+        return count;
+    }   
+
+    public int[] seive(int n){
+        int[] arr = new int[n+1];
+        Arrays.fill(arr,1);
+
+        if(n >= 0) arr[0] = 0;
+        if(n >= 1) arr[1] = 0;
+
+        for(int i=2; i*i<=n; i++){
+            if(arr[i] == 1){      // i is prime
+               for(int j=i*i; j<=n; j+=i){
+                arr[j] = 0;  //composite
+               }
+            }
+        }
+        return arr;
+    }
+}
