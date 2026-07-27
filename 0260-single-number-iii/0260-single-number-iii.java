8c550b887e7b2b737a7e2ffdbe15c80f69ec50bf
Deepanshu Singh <deepanshusingh202206@gmail.com>
2026-07-28 01:35:25 +0530

    Time: 2 ms (43.22%), Space: 48 MB (40.87%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] singleNumber(int[] arr) {
+        int xor = 0;
+        for(int ele : arr ){
+            xor ^= ele;
+        }
+        int mask = (xor&(xor-1))^xor;
+        int b1 = 0, b2 = 0;
+        for(int ele: arr){
+            if((ele & mask) != 0) b1 ^= ele;
+            else b2 ^= ele;
+        }
+        int[] ans = {b1,b2};
+        Arrays.sort(ans);
+        return ans;
+    }
+}
+
+//we can do it using hashmap and hashset also but when if space constraint is not given
