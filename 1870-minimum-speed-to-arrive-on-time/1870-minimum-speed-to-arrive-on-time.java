83f58da63b1bb29640c0c616a9a4ac8778d56dc3
Deepanshu Singh <deepanshusingh202206@gmail.com>
2026-08-04 01:21:13 +0530

    Time: 124 ms (68.95%), Space: 99.4 MB (61.82%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        if(hour <= dist.length-1)
+        return -1;
+        
+        int max = Integer.MIN_VALUE;
+        for(int ele : dist){
+            max = Math.max(max,ele);
+        }
+        int lo = 1, hi = 10000000, speed = max;
+        while(lo <= hi){       
+            int mid = lo + (hi-lo)/2;
+            if(hours(mid,dist) <= hour){
+                hi = mid-1;
+                speed = mid;
+            }
+            else lo = mid +1 ;
+        }
+        return speed;
+    }
+
+    private double hours(int speed, int[] dist){
+        double time = 0;
+        for(int i=0; i<dist.length-1 ; i++){
+            double t = (double)dist[i]/speed;
+            time += Math.ceil(t);
+        }
+
+//handling last case 
+
+        time += (double)dist[dist.length -1]/speed ;
+        return  time;
+    }
+}
+
+// each train can only depart at integer hour
